3856eaa79df6ea49c7edd01789a0d7ad8a992ce9
prabhu-raja <prabhuraja@gmail.com>
2018-08-07 16:47:45 -0400

    added multiple components statically

---
+++ src/components/Order.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Order extends React.Component {
+    render() {
+        return (
+            <div className="order">
+                Order!! <span>👨🏻‍⚖️</span>
+            </div>
+        )
+    }
+}
+
+export default Order;
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p> Hello! </p>
+        return (
+            <form className="store-selector">
+                <h2>Please enter a Store</h2>
+                <input type="text" required placeholder="Store Name" />
+                <button type="submit">Visit Store 👉</button>
+            </form>
+        )
     }
 }
 
-export default StorePicker
+export default StorePicker;
