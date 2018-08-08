c4012b4fd8fef6f63de8a2e64c57770db83dcb46
prabhu-raja <prabhuraja@gmail.com>
2018-08-07 20:24:35 -0400

    8- using ES6 instead of class components

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header tagline="Fresh 🌊 🐟🦈🦀 Market" />
+                    <Header tagline="Fresh 🌊 🐟🦈🦀 Market 6" />
                 </div>
                 <Order />
                 <Inventory />
