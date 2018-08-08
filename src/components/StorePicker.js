1291543e1d96e5b919dfef16ecd994be9bcfb1b6
prabhu-raja <prabhuraja@gmail.com>
2018-08-08 19:22:07 -0400

    use constructor tobind this in all methods

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,10 +2,20 @@ import React from 'react';
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+
+    constructor() {
+        super();
+        this.goToStore = this.goToStore.bind(this);
+        console.log('I am from Constr');
+    }
+
+    myInput = React.createRef();
+
     goToStore(event) {
         // 1. Stop the form from Submitting
         event.preventDefault();
         // 2. get the text from that input
+        console.log('🏬', this);
 
         // 3. Change the page to  /store/whatever-they-entered
     }
@@ -15,6 +25,7 @@ class StorePicker extends React.Component {
                 <h2>Please enter a Store</h2>
                 <input 
                     type="text" 
+                    ref={this.myInput}
                     required 
                     placeholder="Store Name" 
                     defaultValue={getFunName()} />
