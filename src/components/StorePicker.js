983cf99c4d10977a7dcbd5f1475dad2faa5b2eb2
prabhu-raja <prabhuraja@gmail.com>
2018-08-08 19:05:36 -0400

    adding prevent default for for submit

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+    goToStore(event) {
+        // 1. Stop the form from Submitting
+        event.preventDefault();
+        // 2. get the text from that input
+
+        // 3. Change the page to  /store/whatever-they-entered
+    }
     render() {
-        console.log('🇨🇦', this);
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please enter a Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+                <input 
+                    type="text" 
+                    required 
+                    placeholder="Store Name" 
+                    defaultValue={getFunName()} />
                 <button type="submit">Visit Store 👉</button>
             </form>
         )
