fbb0f601ca432cd2764203e08d93bd5c592632db
prabhu-raja <prabhuraja@gmail.com>
2018-08-08 16:29:20 -0400

    using helper functions

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
     render() {
+        console.log('🇨🇦', this);
         return (
             <form className="store-selector">
                 <h2>Please enter a Store</h2>
-                <input type="text" required placeholder="Store Name" />
+                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
                 <button type="submit">Visit Store 👉</button>
             </form>
         )
